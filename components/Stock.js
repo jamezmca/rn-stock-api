ba6e692216581832905903155e15310da4c59637
James McArthur <jamesmca@live.com>
2021-02-27 12:37:19 -0800

    Couldn't get the input to work
    RESTARTED :'(

--- components/Stock.js
+++ components/Stock.js
@@ -1,6 +1,13 @@
 import React, { Component, useEffect, useState } from 'react';
 import { Dimensions, StyleSheet, Text, View } from 'react-native';
-
+import {
+    LineChart,
+    BarChart,
+    PieChart,
+    ProgressChart,
+    ContributionGraph,
+    StackedBarChart
+} from "react-native-chart-kit";
 
 
 const Stock = () => {
@@ -26,11 +33,49 @@ const Stock = () => {
                 };
             })
             .catch((error) => console.error(error));
+            
     };
+    
 
     return (
-        <View style={styles.stockContainer}>
-            
+        <View>
+            <Text>Bezier Line Chart</Text>
+            <LineChart
+                data={{
+                    labels: xValues,
+                    datasets: [
+                        {
+                            data: yValues
+                        }
+                    ]
+                }}
+                width={Dimensions.get("window").width} // from react-native
+                height={220}
+                yAxisLabel="$"
+                yAxisSuffix="k"
+                yAxisInterval={1} // optional, defaults to 1
+                chartConfig={{
+                    backgroundColor: "#e26a00",
+                    backgroundGradientFrom: "#fb8c00",
+                    backgroundGradientTo: "#ffa726",
+                    decimalPlaces: 2, // optional, defaults to 2dp
+                    color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                    labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                    style: {
+                        borderRadius: 16
+                    },
+                    propsForDots: {
+                        r: "6",
+                        strokeWidth: "2",
+                        stroke: "#ffa726"
+                    }
+                }}
+                bezier
+                style={{
+                    marginVertical: 8,
+                    borderRadius: 16
+                }}
+            />
         </View>
     );
 };
